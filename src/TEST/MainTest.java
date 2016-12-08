f1fb17dfe85eb9aaf206a0afbf0f55a6402dc1bb
DESKTOP-E6MVUQI\LeeDongYoung <dyl159@naver.com>
2016-12-08 23:15:56 +0900

    main test 90% 완료

--- src/TEST/MainTest.java
+++ src/TEST/MainTest.java
@@ -7,8 +7,16 @@ public class MainTest {
     @Test
     public void testMain(){
         String dir = System.getProperty("user.dir");
-        String[] s = {"-i","a.md"};
+        String[] s1 = {"-i","a.md","-o","a.html","-s","plain"};
+        String[] s2 = {"-help"};
+        String[] s3 = {"-i","a.md","-s","plain"};
+        String[] s4 = {"-i","a.md","-o","a.html"};
+        String[] s5 = {"-i","a.md","-s","plain","-o","a.html"};
+
         Main m = new Main();
-        m.main(s);
+        m.main(s1);
+        m.main(s2);
+        m.main(s3);
+        m.main(s4);
     }
 }
