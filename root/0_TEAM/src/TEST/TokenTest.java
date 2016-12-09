67de66989a16865b65be4c243dd6850f999d16e1
darkelode <21200610@handong.edu>
2016-12-09 23:20:14 +0900

    Update TokenTest.java

--- root/0_TEAM/src/TEST/TokenTest.java
+++ root/0_TEAM/src/TEST/TokenTest.java
@@ -14,14 +14,14 @@ public class TokenTest {
 	static Token tok = new Token();
 
 	static PlainVisitor pVisitor = new PlainVisitor();
-	static FancyVisitor fVisitor = new FancyVisitor();
+//	static FancyVisitor fVisitor = new FancyVisitor();
 	static Token tok1 = new Token();
 	static Token tok2 = new Token();
 
 	@BeforeClass public static void justBefore(){
 	    tok = Token.create(in);
 		tok1.accept(pVisitor);
-		tok2.accept(fVisitor);
+//		tok2.accept(fVisitor);
 	}
 	
 	@Test
